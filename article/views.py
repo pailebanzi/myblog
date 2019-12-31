4d1390ec71d816c8abd1e261017dac34e602b2ff
wangdaxue <pailebanzi@gmail.com>
2019-12-31 11:19:46 +0800

    xxx

--- article/views.py
+++ article/views.py
@@ -1,6 +1,15 @@
 # 导入 HttpResponse 模块
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import ArticlePost
+
 
 # 视图函数
 def article_list(request):
-    return HttpResponse("Hello World!")
+    def article_list(request):
+    # 取出所有博客文章
+    articles = ArticlePost.objects.all()
+    # 需要传递给模板（templates）的对象
+    context = { 'articles': articles }
+    # render函数：载入模板，并返回context对象
+    return render(request, 'article/list.html', context)
